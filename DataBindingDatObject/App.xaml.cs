aa97a74fab2e8273badbb31f4f6377a6f30d52b4
Kendra Aurora Cach Pool <24470033.campeche.tecnm.mx>
2025-10-29 11:18:21 -0600

    Data Binding

---
+++ DataBindingDatObject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBindingDatObject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DataBindingDatObject/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DataBindingDatObject.Models;
+
+namespace DataBindingDatObject
+{
+    public partial class MainPage : ContentPage
+    {
+        private Contador contador;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = new Contador();
+            BindingContext = contador;           
+        }
+
+        private void OnReiniciarButtonClicked(object sender, EventArgs e)
+        {
+            contador.Reiniciar();
+        }
+
+        private void OnContarButtonClicked(object sender, EventArgs e)
+        {
+            contador.Contar();
+        }
+    }
+
+}
