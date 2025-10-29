a2f0914f004bdc1562d09a81add7c9613fda1856
Kendra Aurora Cach Pool <24470033.campeche.tecnm.mx>
2025-10-29 11:45:36 -0600

    Ejemplo de sincronización de elementos Xaml con manejo de elementos

---
+++ DataBinding.XamlElement/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DataBinding.XamlElement
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredTextLabel.Text=TextEntry.Text;
+        }
+
+        private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
